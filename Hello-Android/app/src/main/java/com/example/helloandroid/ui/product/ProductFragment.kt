3a0d48c14fce6335a7946302de6410dac9457166
Kotlintpoint <ankit.sodha123@gmail.com>
2025-05-15 08:51:38 +0530

    Drawer with Customer, Vendor, Product
    - Adding new Fragments for Customer, Vendor, Product
    - Adding Menu using MenuProvider

---
+++ Hello-Android/app/src/main/java/com/example/helloandroid/ui/product/ProductFragment.kt
@@ -0,0 +1,55 @@
+package com.example.helloandroid.ui.product
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.example.helloandroid.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ProductFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ProductFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object: MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.drawer_dashboard, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                if(menuItem.itemId == R.id.action_new){
+                    val navController = activity?.findNavController(R.id.nav_host_fragment_content_drawer_dashboard)
+                    navController?.navigate(R.id.action_nav_product_to_nav_new_product)
+                    return true
+                }
+                return false
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+}
