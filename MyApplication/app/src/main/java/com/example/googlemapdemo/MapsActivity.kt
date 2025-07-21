e3c14b592672d5101b728a61ca4988eb9397b040
Kotlintpoint <ankit.sodha123@gmail.com>
2025-07-21 10:38:54 +0530

    Google Map

---
+++ MyApplication/app/src/main/java/com/example/googlemapdemo/MapsActivity.kt
@@ -0,0 +1,152 @@
+package com.example.googlemapdemo
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.annotation.RequiresPermission
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.googlemapdemo.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.CameraPosition
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
+    ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private val LOCATION_PERMISSION_REQUEST_CODE: Int = 10
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        val mountainView = LatLng(37.4, -122.1)
+
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.addMarker(MarkerOptions().position(mountainView).title("Marker in MountainView"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15f))
+
+
+        // Construct a CameraPosition focusing on Mountain View and animate the camera to that position.
+        val cameraPosition = CameraPosition.Builder()
+            .target(mountainView) // Sets the center of the map to Mountain View
+            .zoom(17f)            // Sets the zoom
+            .bearing(90f)         // Sets the orientation of the camera to east
+            .tilt(30f)            // Sets the tilt of the camera to 30 degrees
+            .build()              // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.uiSettings.isCompassEnabled = true
+        mMap.uiSettings.isMyLocationButtonEnabled = true
+        enableMyLocation()
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
+
+        // 1. Check if permissions are granted, if so, enable the my location layer
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            mMap.isMyLocationEnabled = true
+            return
+        }
+
+        // 2. If if a permission rationale dialog should be shown
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        ) {
+//            PermissionUtils.RationaleDialog.newInstance(
+//                LOCATION_PERMISSION_REQUEST_CODE, true
+//            ).show(supportFragmentManager, "dialog")
+            return
+        }
+
+        // 3. Otherwise, request permission
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            LOCATION_PERMISSION_REQUEST_CODE
+        )
+    }
+
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<String>,
+//        grantResults: IntArray
+//    ) {
+//        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+//            super.onRequestPermissionsResult(
+//                requestCode,
+//                permissions,
+//                grantResults
+//            )
+//            return
+//        }
+//
+//        if (isPermissionGranted(
+//                permissions,
+//                grantResults,
+//                Manifest.permission.ACCESS_FINE_LOCATION
+//            ) || isPermissionGranted(
+//                permissions,
+//                grantResults,
+//                Manifest.permission.ACCESS_COARSE_LOCATION
+//            )
+//        ) {
+//            // Enable the my location layer if the permission has been granted.
+//            enableMyLocation()
+//        } else {
+//            // Permission was denied. Display an error message
+//            // Display the missing permission error dialog when the fragments resume.
+//            permissionDenied = true
+//        }
+//    }
+}
